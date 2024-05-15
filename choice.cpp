b2b567989374d75be282237441f5da25cc8346d3
Shiva823 <159522462+Shiva823@users.noreply.github.com>
2024-05-15 22:09:12 +0545

    Update choice.cpp

--- choice.cpp
+++ choice.cpp
@@ -1,30 +1,30 @@
 #include<iostream>
 using namespace std;
-float add(int a,int b);
-float sub(int a, int b);
-float mul(int a, int b);
-float dvd(int a,int b);
-int main(){
-    int p,q,ch;
+float add(float a,float b);
+float sub(float a,float b);
+float mul(float a, float b);
+float dvd(float a,float b);
+float main(){
+    float p,q,ch;
     cout<<"enter any two numbers";
     cin>>p>>q;
     cout<<"enter your choice 1:add  2:subtract  3:multiply  4:divide";
     cin>>ch;
     switch(ch){
         case 1 :
-                add(p,q);
+                
                 cout<<"the sum is "<<add(p,q);
                 break;
         case 2 :
-                sub(p,q);
+                
                 cout<<"the subtraction  is"<<sub(p,q);
                 break;
         case 3 :
-                mul(p,q);
+                
                 cout<<"the multiplication is "<<mul(p,q);
                 break;
         case 4 :
-                dvd(p,q);
+                
                 cout<<"the division is"<<dvd(p,q);
                 break;
         default :
@@ -34,18 +34,18 @@ int main(){
     
 
     }
-       float add(int a,int b){
+       float add(float a,float b){
             return a+b;
         }
 
-        float sub(int a, int b){
+        float sub(float a, float b){
             return a-b;
         }
         
-        float mul(int a,int b){
+        float mul(float a,float b){
             return a*b;
         }
 
-        float dvd(int a ,int b){
+        float dvd(float a ,float b){
             return a/b;
         }
